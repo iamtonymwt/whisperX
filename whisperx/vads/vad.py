599e4d1b339c9f6546d0dfadc9a7b65efccad1b1
wentao <tonyyyma@gmail.com>
2025-07-29 18:42:45 +0000

    add new function

--- whisperx/vads/vad.py
+++ whisperx/vads/vad.py
@@ -55,20 +55,24 @@ class Vad:
     # keep merge_chunks as static so it can be also used by manually assigned vad_model (see 'load_model')
     @staticmethod
     def merge_chunks_music(segments,
-                     chunk_size,
-                     onset: float,
-                     offset: Optional[float]):
-        """
-         Merge operation described in paper
-         """
+                        chunk_size,
+                        onset: float,
+                        offset: Optional[float],
+                        silence_gap=1.0,
+                        short_segment_threshold=3.0
+                        ):
         curr_end = 0
         merged_segments = []
-        seg_idxs: list[tuple]= []
+        seg_idxs: list[tuple] = []
         speaker_idxs: list[Optional[str]] = []
 
         curr_start = segments[0].start
         for seg in segments:
-            if seg.end - curr_start > chunk_size and curr_end - curr_start > 0:
+            # new logic: avoid silence gaps
+            has_silence_gap = (seg.start - curr_end) > silence_gap
+            exceeds_chunk = (seg.end - curr_start) > chunk_size
+
+            if (exceeds_chunk or has_silence_gap) and curr_end - curr_start > 0:
                 merged_segments.append({
                     "start": curr_start,
                     "end": curr_end,
@@ -77,15 +81,23 @@ class Vad:
                 curr_start = seg.start
                 seg_idxs = []
                 speaker_idxs = []
+
             curr_end = seg.end
             seg_idxs.append((seg.start, seg.end))
             speaker_idxs.append(seg.speaker)
+
         # add final
         merged_segments.append({
             "start": curr_start,
             "end": curr_end,
             "segments": seg_idxs,
         })
+
+        # new logic: filter out short segments
+        merged_segments = [
+            chunk for chunk in merged_segments
+            if (chunk["end"] - chunk["start"]) >= short_segment_threshold
+        ]
 
         return merged_segments
 
