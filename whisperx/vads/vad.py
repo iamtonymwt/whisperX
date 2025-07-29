051e060e3a1c1f2b04d2dbaf575a4614103d0a73
wentao <tonyyyma@gmail.com>
2025-07-29 18:54:46 +0000

    test

--- whisperx/vads/vad.py
+++ whisperx/vads/vad.py
@@ -61,7 +61,7 @@ class Vad:
                         silence_gap=1.0,
                         short_segment_threshold=3.0
                         ):
-        print(f"segments: {segments}")
+        print(f"segments: {segments[0]}")
         curr_end = 0
         merged_segments = []
         seg_idxs: list[tuple] = []
