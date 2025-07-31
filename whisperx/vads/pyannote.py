fa2a93dc32f43fcc1a54249688ada089dcb85afd
wentao <tonyyyma@gmail.com>
2025-07-31 16:03:33 +0000

    add print

--- whisperx/vads/pyannote.py
+++ whisperx/vads/pyannote.py
@@ -282,4 +282,6 @@ class Pyannote(Vad):
             print("No active speech found in audio")
             return []
         assert segments_list, "segments_list is empty."
+        for segment in segments_list:
+            print(f"Segment: {segment.start:.2f} - {segment.end:.2f}")
         return Vad.merge_chunks_music(segments_list, chunk_size, onset, offset, silence_gap, short_segment_threshold)
